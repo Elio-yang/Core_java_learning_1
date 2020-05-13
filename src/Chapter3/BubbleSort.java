dfb64981b8512a40d272c9b47a178f94d828fd17
Elio-yang <2925539854@qq.com>
2020-05-13 20:58:07 +0800

    code-style change

--- src/Chapter3/BubbleSort.java
+++ src/Chapter3/BubbleSort.java
@@ -1,12 +1,13 @@
-package learn;
+package Chapter3;
 
 /**
- * 2-Darray
+ * 2-D_array
  */
 public class BubbleSort {
 
     public static void main(String[] args) {
-        int[] arr=new int[]{1,6,4,7,2,9,4};
+        int[] arr;
+        arr = new int[]{1,6,4,7,2,9,4};
         for(int i=0;i<arr.length-1;i++){
             for(int j=0;j<arr.length-1-i;j++){
                 if(arr[j]>arr[j+1]){
@@ -16,8 +17,8 @@ public class BubbleSort {
                 }
             }
         }
-        for(int i=0;i<arr.length;i++){
-        System.out.print("  "+arr[i]);
+        for (int value : arr) {
+            System.out.print("  " + value);
         }
     }
 }
